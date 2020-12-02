3e81c69f6c2dae5e0485f6ac800c1bae826679e6
Carlos Mertens <mertenscarlos@yahoo.com>
2020-12-03 00:19:51 +0100

    Add images to the project

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from 'next/head';
+import Image from 'next/image';
 
 export default function Home() {
   return (
@@ -12,6 +13,16 @@ export default function Home() {
         <h1 className='text-5xl text-amber-500 text-center'>
           Welcome to Diazno 2.0
         </h1>
+        <Image
+          src='/hero.jpg'
+          alt='Diazno backgroung image'
+          width={2000}
+          height={2000}
+          layout='responsive'
+          sizes='100vw'
+          priority
+        />
+        <img src='logo.png' />
       </main>
     </div>
   );
