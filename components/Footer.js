b0d16bda3a3555b6486d424cabff51f6aa41f6e8
Carlos Mertens <mertenscarlos@yahoo.com>
2021-02-18 12:53:25 +0100

    Style Footer/SocialMedia components

--- components/Footer.js
+++ components/Footer.js
@@ -4,10 +4,10 @@ import SocialMedia from './util/SocialMedia';
 export const Footer = () => {
   return (
     <>
-      <footer className='fixed bg-emperor inset-x-0 bottom-0 flex flex-col leading-3 justify-center items-center py-2'>
+      <footer className='fixed bg-mine-shaft inset-x-0 bottom-0 flex flex-col leading-3 justify-center items-center py-2'>
         <SocialMedia />
         <br />
-        <p className='text-white'>
+        <p className='text-white cursor-default'>
           &copy; {new Date().getFullYear()} All rights reserved.
         </p>
       </footer>
--- components/util/SocialMedia.js
+++ components/util/SocialMedia.js
@@ -13,28 +13,28 @@ const ShareSocial = ({ transparent }) => {
         rel='noreferrer noopener'
         href='https://www.facebook.com/officialdiazno'
         className='px-2 focus:text-blue-400 focus:outline-none'>
-        <FaFacebookSquare className='hover:text-blue-400 ' />
+        <FaFacebookSquare className='hover:text-blue-400 text-2xl transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110' />
       </a>
       <a
         target='_blank'
         rel='noreferrer noopener'
         href='https://www.youtube.com/channel/UCuAuh0pongpsiMQGxwLcTxQ'
         className='px-2 focus:text-red-500 focus:outline-none'>
-        <FaYoutube className='hover:text-red-500' />
+        <FaYoutube className='hover:text-red-500 text-2xl transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110' />
       </a>
       <a
         target='_blank'
         rel='noreferrer noopener'
         href='https://www.instagram.com/diazno/'
         className='px-2 focus:text-red-300 focus:outline-none'>
-        <FaInstagram className='hover:text-red-300' />
+        <FaInstagram className='hover:text-red-200 text-2xl transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110' />
       </a>
       <a
         target='_blank'
         rel='noreferrer noopener'
         href='https://open.spotify.com/artist/4UobcfdsvVwgXka22f9I7d?si=PXsGbhijT3qVRajiC6jVvw'
         className='px-2 focus:text-green-400 focus:outline-none'>
-        <FaSpotify className='hover:text-green-400' />
+        <FaSpotify className='hover:text-green-400 text-2xl transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110' />
       </a>
     </div>
   );
