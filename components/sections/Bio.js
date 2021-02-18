32359b50b8af24014a1349922789fb8d752b57ed
Carlos Mertens <mertenscarlos@yahoo.com>
2021-02-18 18:13:50 +0100

    Create Bio component

--- components/sections/Bio.js
+++ components/sections/Bio.js
@@ -1,4 +1,5 @@
 import { SideHeading } from '../util/SideHeading';
+import { BioText } from '../util//BioText';
 
 export const Bio = () => {
   return (
@@ -9,7 +10,12 @@ export const Bio = () => {
         <div className=''>
           <SideHeading title='Bio' textColor='white' barsColor='blue-300' />
         </div>
-        <div className=''></div>
+        <div className=''>
+          <BioText />
+          <div className='flex items-center justify-center'>
+            <h3 className='text-2xl uppercase'>Stay up to date!</h3>
+          </div>
+        </div>
       </section>
     </>
   );
