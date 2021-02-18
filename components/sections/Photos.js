7d46eeacac49e75ce527b9a15c066b5006010b62
Carlos Mertens <mertenscarlos@yahoo.com>
2021-02-18 17:45:38 +0100

    Create Photos component

--- components/sections/Photos.js
+++ components/sections/Photos.js
@@ -9,7 +9,14 @@ export const Photos = () => {
         <div className=''>
           <SideHeading title='Photos' textColor='white' barsColor='yellow-300' />
         </div>
-        <div className=''></div>
+        <div className='grid grid-row-6 md:grid-rows-3 md:grid-cols-2 lg:grid-rows-2 lg:grid-cols-3 p-4'>
+          <div className='bg-pink-50'>1</div>
+          <div className='bg-pink-200'>2</div>
+          <div className='bg-pink-300'>3</div>
+          <div className='bg-pink-400'>4</div>
+          <div className='bg-pink-500'>5</div>
+          <div className='bg-pink-600'>6</div>
+        </div>
       </section>
     </>
   );
--- components/sections/Store.js
+++ components/sections/Store.js
@@ -10,12 +10,12 @@ export const Store = () => {
           <SideHeading title='Store' textColor='white' barsColor='green-400' />
         </div>
         <div className='grid grid-row-6 gap-4 md:grid-rows-3 md:grid-cols-2 lg:grid-rows-2 lg:grid-cols-3 p-4'>
-          <div className='bg-pink-50'>1</div>
-          <div className='bg-pink-50'>2</div>
-          <div className='bg-pink-50'>3</div>
-          <div className='bg-pink-50'>4</div>
-          <div className='bg-pink-50'>5</div>
-          <div className='bg-pink-50'>6</div>
+          <div className='bg-green-50'>1</div>
+          <div className='bg-green-200'>2</div>
+          <div className='bg-green-300'>3</div>
+          <div className='bg-green-400'>4</div>
+          <div className='bg-green-500'>5</div>
+          <div className='bg-green-600'>6</div>
         </div>
       </section>
     </>
