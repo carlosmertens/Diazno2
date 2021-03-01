f5e0daab928cffafb7bccab41395fd2913d083fb
Carlos Mertens <mertenscarlos@yahoo.com>
2021-03-01 20:00:07 +0100

    Update Music component

--- components/sections/Music.js
+++ components/sections/Music.js
@@ -35,29 +35,29 @@ export const Music = () => {
               allowFullScreen></iframe>
           </div>
           <div className='flex items-center justify-center'>
-            <h2 className='text-xl md:text-2xl lg:text-4xl uppercase'>
+            <h2 className='text-white text-xl md:text-3xl lg:text-4xl uppercase'>
               Listen on &nbsp;
             </h2>
             <a
               target='_blank'
               rel='noreferrer noopener'
               href='https://www.vimeo.com'
-              className='px-2 focus:text-blue-400 focus:outline-none'>
-              <FaVimeo className='hover:text-blue-300 text-2xl transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110' />
+              className='px-2 text-blue-400 focus:outline-none'>
+              <FaVimeo className='hover:text-blue-200 text-3xl transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110' />
             </a>
             <a
               target='_blank'
               rel='noreferrer noopener'
               href='https://open.spotify.com/artist/4UobcfdsvVwgXka22f9I7d?si=PXsGbhijT3qVRajiC6jVvw'
-              className='px-2 focus:text-green-400 focus:outline-none'>
-              <FaSpotify className='hover:text-green-400 text-2xl transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110' />
+              className='px-2 text-green-400 focus:outline-none'>
+              <FaSpotify className='hover:text-green-200 text-3xl transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110' />
             </a>
             <a
               target='_blank'
               rel='noreferrer noopener'
               href='https://www.youtube.com/channel/UCuAuh0pongpsiMQGxwLcTxQ'
-              className='px-2 focus:text-red-500 focus:outline-none'>
-              <FaYoutube className='hover:text-red-500 text-3xl transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110' />
+              className='px-2 text-red-500 focus:outline-none'>
+              <FaYoutube className='hover:text-red-200 text-3xl transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110' />
             </a>
           </div>
         </div>
