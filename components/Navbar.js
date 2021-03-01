af8784104ee830e544b6f4378d61fde9e82d1f48
Carlos Mertens <mertenscarlos@yahoo.com>
2021-03-01 19:54:40 +0100

    Update Navbar component

--- components/Navbar.js
+++ components/Navbar.js
@@ -4,17 +4,19 @@ import { useState } from 'react';
 export const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
   return (
-    <nav className=' bg-transparent md:flex md:items-center md:justify-between'>
+    <nav className='fixed flex items-center justify-between'>
       <div className='flex items-center justify-between pl-8 py-6'>
-        <div>
-          <h1 className='text-6xl text-mine-shaft cursor-default transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110'>
-            D!azno
-          </h1>
+        <div className='p-4'>
+          <Link href='/'>
+            <a className='text-7xl text-white font-normal tracking-widest	hover:text-mine-shaft'>
+              D!AZNO
+            </a>
+          </Link>
         </div>
 
         <div className='md:hidden pr-8'>
           <button
-            className='text-mine-shaft hover:text-white focus:outline-none transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110'
+            className='text-white hover:text-mine-shaft focus:outline-none transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110'
             type='button'
             onClick={() => {
               setIsOpen(!isOpen);
@@ -50,32 +52,32 @@ export const Navbar = () => {
           isOpen ? 'block' : 'hidden'
         } px-2 pl-6 pt-2 pb-8 md:flex md:items-center md:justify-between`}>
         <Link href='/'>
-          <a className='block mt-4 px-3 py-1 text-mine-shaft uppercase rounded hover:text-white focus:text-mine-shaft'>
+          <a className='block mt-4 px-3 py-1 text-white uppercase font-bold tracking-widest rounded hover:text-mine-shaft'>
             Music & Videos
           </a>
         </Link>
         <Link href='/'>
-          <a className='block mt-4 px-3 py-1 text-mine-shaft uppercase rounded hover:text-white focus:text-mine-shaft'>
-            Tours
+          <a className='block mt-4 px-3 py-1 text-white uppercase font-bold tracking-widest rounded hover:text-mine-shaft'>
+            Tour
           </a>
         </Link>
         <Link href='/'>
-          <a className='block mt-4 px-3 py-1 text-mine-shaft uppercase rounded hover:text-white focus:text-mine-shaft'>
+          <a className='block mt-4 px-3 py-1 text-white uppercase font-bold tracking-widest rounded hover:text-mine-shaft'>
             Photos
           </a>
         </Link>
         <Link href='/'>
-          <a className='block mt-4 px-3 py-1 text-mine-shaft uppercase rounded hover:text-white focus:text-mine-shaft'>
+          <a className='block mt-4 px-3 py-1 text-white uppercase font-bold tracking-widest rounded hover:text-mine-shaft'>
             Store
           </a>
         </Link>
         <Link href='/'>
-          <a className='block mt-4 px-3 py-1 text-mine-shaft uppercase rounded hover:text-white focus:text-mine-shaft'>
+          <a className='block mt-4 px-3 py-1 text-white uppercase font-bold tracking-widest rounded hover:text-mine-shaft'>
             Bio
           </a>
         </Link>
         <Link href='/'>
-          <a className='block mt-4 px-3 pr-5 py-1 text-mine-shaft uppercase rounded hover:text-white focus:text-mine-shaft'>
+          <a className='block mt-4 px-3 pr-5 py-1 text-white uppercase font-bold tracking-widest rounded hover:text-mine-shaft'>
             Contact
           </a>
         </Link>
