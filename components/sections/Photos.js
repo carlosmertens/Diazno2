8e1bf69295dae8bd4aa444319d15120d71429060
Carlos Mertens <mertenscarlos@yahoo.com>
2021-03-11 23:50:51 +0100

    Create list of photos

--- components/sections/Photos.js
+++ components/sections/Photos.js
@@ -3,41 +3,104 @@ import { SectionHeading } from '../util/SectionHeading';
 export const Photos = () => {
   return (
     <section id='photos-section' className='min-h-screen gap-0 bg-emperor'>
-      <div className=''>
-        <SectionHeading
-          title='Photos'
-          textColor='yellow-400'
-          svgIcon={
-            <svg
-              className='h-10 w-10 mr-2 text-yellow-400'
-              xmlns='http://www.w3.org/2000/svg'
-              fill='none'
-              viewBox='0 0 24 24'
-              stroke='currentColor'>
-              <path
-                strokeLinecap='round'
-                strokeLinejoin='round'
-                strokeWidth={2}
-                d='M3 9a2 2 0 012-2h.93a2 2 0 001.664-.89l.812-1.22A2 2 0 0110.07 4h3.86a2 2 0 011.664.89l.812 1.22A2 2 0 0018.07 7H19a2 2 0 012 2v9a2 2 0 01-2 2H5a2 2 0 01-2-2V9z'
-              />
-              <path
-                strokeLinecap='round'
-                strokeLinejoin='round'
-                strokeWidth={2}
-                d='M15 13a3 3 0 11-6 0 3 3 0 016 0z'
-              />
-            </svg>
-          }
-          // barsColor='yellow-300'
-        />
-      </div>
-      <div className='grid grid-row-6 md:grid-rows-3 md:grid-cols-2 lg:grid-rows-2 lg:grid-cols-3 p-4'>
-        <div className='bg-pink-50'>1</div>
-        <div className='bg-pink-200'>2</div>
-        <div className='bg-pink-300'>3</div>
-        <div className='bg-pink-400'>4</div>
-        <div className='bg-pink-500'>5</div>
-        <div className='bg-pink-600'>6</div>
+      <SectionHeading
+        title='Photos'
+        textColor='yellow-400'
+        svgIcon={
+          <svg
+            className='h-10 w-10 mr-2 text-yellow-400'
+            xmlns='http://www.w3.org/2000/svg'
+            fill='none'
+            viewBox='0 0 24 24'
+            stroke='currentColor'>
+            <path
+              strokeLinecap='round'
+              strokeLinejoin='round'
+              strokeWidth={2}
+              d='M3 9a2 2 0 012-2h.93a2 2 0 001.664-.89l.812-1.22A2 2 0 0110.07 4h3.86a2 2 0 011.664.89l.812 1.22A2 2 0 0018.07 7H19a2 2 0 012 2v9a2 2 0 01-2 2H5a2 2 0 01-2-2V9z'
+            />
+            <path
+              strokeLinecap='round'
+              strokeLinejoin='round'
+              strokeWidth={2}
+              d='M15 13a3 3 0 11-6 0 3 3 0 016 0z'
+            />
+          </svg>
+        }
+        // barsColor='yellow-300'
+      />
+
+      <div className='mx-auto py-12 px-4 max-w-7xl sm:px-6 lg:px-8 lg:py-24'>
+        <div className='space-y-12'>
+          <ul className='space-y-12 sm:grid sm:grid-cols-2 lg:grid-cols-3 lg:gap-x-8'>
+            <li>
+              <div className='space-y-4'>
+                <div className='aspect-w-4 aspect-h-3'>
+                  <img
+                    className='object-cover shadow-lg rounded-lg'
+                    src='/img/pic2.jpg'
+                    alt='Photo2'
+                  />
+                </div>
+              </div>
+            </li>
+            <li>
+              <div className='space-y-4'>
+                <div className='aspect-w-4 aspect-h-3'>
+                  <img
+                    className='object-cover shadow-lg rounded-lg'
+                    src='/img/pic3.jpg'
+                    alt='Photo3'
+                  />
+                </div>
+              </div>
+            </li>
+            <li>
+              <div className='space-y-4'>
+                <div className='aspect-w-4 aspect-h-3'>
+                  <img
+                    className='object-cover shadow-lg rounded-lg'
+                    src='/img/pic4.jpg'
+                    alt='Photo4'
+                  />
+                </div>
+              </div>
+            </li>
+            <li>
+              <div className='space-y-4'>
+                <div className='aspect-w-4 aspect-h-3'>
+                  <img
+                    className='object-cover shadow-lg rounded-lg'
+                    src='/img/pic5.jpg'
+                    alt='Photo5'
+                  />
+                </div>
+              </div>
+            </li>
+            <li>
+              <div className='space-y-4'>
+                <div className='aspect-w-4 aspect-h-3'>
+                  <img
+                    className='object-cover shadow-lg rounded-lg'
+                    src='/img/pic6.jpg'
+                    alt='Photo6'
+                  />
+                </div>
+              </div>
+            </li>
+            <li>
+              <div className='space-y-4'>
+                <div className='aspect-w-4 aspect-h-3'>
+                  <img
+                    className='object-cover shadow-lg rounded-lg'
+                    src='/img/pic1.jpg'
+                    alt='Photo1'
+                  />
+                </div>
+              </div>
+            </li>
+          </ul>
+        </div>
       </div>
     </section>
   );
