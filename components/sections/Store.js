e227e2f3d23caf459e0678f0c482a0fb32501fad
Carlos Mertens <mertenscarlos@yahoo.com>
2021-09-17 23:17:16 +0200

    Style store section

--- components/sections/Store.js
+++ components/sections/Store.js
@@ -22,16 +22,15 @@ export const Store = () => {
               />
             </svg>
           }
-          // barsColor='green-400'
         />
       </div>
-      <div className='grid grid-row-6 gap-4 md:grid-rows-3 md:grid-cols-2 lg:grid-rows-2 lg:grid-cols-3 p-4'>
-        <div className='bg-green-50'>1</div>
-        <div className='bg-green-200'>2</div>
-        <div className='bg-green-300'>3</div>
-        <div className='bg-green-400'>4</div>
-        <div className='bg-green-500'>5</div>
-        <div className='bg-green-600'>6</div>
+      <div className='grid grid-row-6 gap-4 pt-10 pb-10 justify-items-center	 md:grid-rows-3 md:grid-cols-2 lg:grid-rows-2 lg:grid-cols-3 p-4'>
+        <div className='h-48 w-3/4 rounded bg-green-50'>1</div>
+        <div className='h-48 w-3/4 rounded bg-green-200'>2</div>
+        <div className='h-48 w-3/4 rounded bg-green-300'>3</div>
+        <div className='h-48 w-3/4 rounded bg-green-400'>4</div>
+        <div className='h-48 w-3/4 rounded bg-green-500'>5</div>
+        <div className='h-48 w-3/4 rounded bg-green-600'>6</div>
       </div>
     </section>
   );
